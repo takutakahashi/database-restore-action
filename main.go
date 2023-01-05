4fa36bd24780249cf8cc5c24dc7e462ca0f4298b
takutaka <takutaka@pepabo.com>
2023-01-05 18:02:28 +0900

    commit

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-func main(){
-	
+import "github.com/takutakahashi/database-restore-action/cmd"
+
+func main() {
+	cmd.Execute()
 }
